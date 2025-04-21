e02fd1b9ac55e5841698bf3ada881c074679287f
Alex Kadai <alexandrkadai@gmail.com>
2025-04-21 23:48:25 +0300

    Task added

--- codewar.js
+++ codewar.js
@@ -501,3 +501,26 @@ function startsWithDigit(str) {
 
 console.log(startsWithDigit("123abc")); // true
 console.log(startsWithDigit("abc123")); // false
+
+//Taks 2 Jokes you've been 'awaiting' for ... promise
+async function sayJoke(apiUrl, jokeId) {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  
+if(!data.jokes) {
+  throw new Error(`No jokes at url: ${apiUrl}`);
+}
+
+  const joke = data.jokes.find(joke => joke.id === jokeId);
+
+  if (!joke) {
+    throw new Error(`No jokes found id: ${jokeId}`);
+  }
+  
+  return {
+    saySetup: function() { return joke.setup},
+    sayPunchLine: function(){ return joke.punchLine}
+  };
+
+}
